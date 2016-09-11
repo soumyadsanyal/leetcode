0900140716b34fa517cfda139518837af86d81b9
Soumya <soumya@soumyadsanyal.com>
2016-09-11 04:20:16 -0400

    .

---
+++ compare_version_numbers.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def compareVersion(self, version1, version2):
+        """
+        :type version1: str
+        :type version2: str
+        :rtype: int
+        """
+        first = self.significant([int(x) for x in version1.split(".")])
+        second = self.significant([int(x) for x in version2.split(".")])
+        if first<second:
+            return -1
+        if first==second:
+            return 0
+        else:
+            return 1
+
+
+    def significant(self, l):
+        while l!=[]:
+            temp = l.pop()
+            if temp==0:
+                continue
+            else:
+                l.append(temp)
+                break
+        return l
