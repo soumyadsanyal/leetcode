213a0ba5b67be176c20588880e9bb76b009da9c2
Soumya <soumya@soumyadsanyal.com>
2016-09-11 20:32:36 -0400

    .

---
+++ longest_palindromic_substring.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        ss = "#" + "#".join(s)+"#"
+        P={}    
+        i=0
+        center=0
+        m=0
+        while i<len(ss):
+            m=2*center-i
+            if m in P and center in P and i+P[m]<center+P[center]:
+                P[i]=P[m]
+            else:
+                P[i]=self.search(ss, i)
+            if i+P[i]>center+P[center]:
+                center=i
+            i+=1
+        Q={P[k]:k for k in P.keys()}
+        bestIndex=Q[max(Q)]
+        bestDiameter=P[bestIndex]
+        return ss[bestIndex-bestDiameter:bestIndex+bestDiameter+1].replace("#", "")
+        
+       
+    def search(self, s, center):
+        i=0
+        rEdge=len(s)-1
+        while True:
+            if center-i-1<0 or center+i+1>rEdge or s[center-i-1]!=s[center+i+1]:
+                return i
+            else:
+                i+=1
